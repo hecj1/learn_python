dd3b39b612be71d77651aee4a9d4a9544cc51f4e
James Howell <james.howell@slalom.com>
2020-08-12 15:35:45 -0400

    Pushing new changes

---
+++ first_numbers.py
@@ -0,0 +1,9 @@
+# Following Chapter 4 example on making Numerical Lists
+# This examply uses the range function in the for loop
+for value in range(1, 5):
+    print(value)
+
+# the Range function doesn't print the second (last) number, you will need increase the last number by n+1 to see the original last number
+
+# Now
+
