93b8dfedde613386cb081d19b67e590c1f09f89b
James Howell <james.howell@slalom.com>
2020-08-16 16:05:28 -0400

    practicing for loops

--- loops.py
+++ loops.py
@@ -1,7 +1,15 @@
-f_list = ["apple", "orange", "banana", "pear", "pecan", "pinapple", "strawberry"]
+# f_list = ["apple", "orange", "banana", "pear", "pecan", "pinapple", "strawberry"]
 
-for i in f_list:
-    print(i)
+# for i in f_list:
+#     print(i)
 
-while i != "pecan":
-    print(i)
+# while i != "pecan":
+#     print(i)
+
+# new = list(range(1,6))
+
+# for number in new:
+#     print(number*2)
+digimon = ['augumon', 'tentomon', 'piyomon', 'gomamon', 'palmon', 'patomon', 'gabumon', 'tailmon']
+for mon in digimon:
+    print(mon, end=' ')
