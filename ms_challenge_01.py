1038b8309c89310798198eb2fdeaeea8a1d3395c
James Howell <james.howell@slalom.com>
2020-08-20 19:48:33 -0400

    adding ms_challeng_01 file

---
+++ ms_challenge_01.py
@@ -0,0 +1,17 @@
+# This program is to complete the first challenge for Create your first Python program
+# the output will be similar to the following sample output:
+# Today's date?
+# Thursday
+# Breakfast calories?
+# 100
+# Lunch calories?
+# 200
+# Dinner calories?
+# 300
+# Snack calories?
+# 400
+# Calorie content for Thursday: 1000
+#Start
+
+# this will ask the user for the date
+print("") 
