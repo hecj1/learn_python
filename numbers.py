44b57b7cc373264a2d81664b46fcfb6ba49d3f33
James Howell <james.howell@slalom.com>
2020-08-20 19:37:15 -0400

    updating numbers.py to complete lesson

--- numbers.py
+++ numbers.py
@@ -1,6 +1,6 @@
-print("First Number:")
-first_number = input()
-print("Second Number:")
-second_number = input()
+print("First Number: ")
+first_number = int(input())
+print("Second Number: ")
+second_number = int(input())
 sum = first_number + second_number
-print(sum)
+print("Sum: " + str(sum))
