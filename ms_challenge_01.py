210eb4e99abce6ca35e49382ad407cc22d286d25
James Howell <james.howell@slalom.com>
2020-08-21 16:57:05 -0400

    completing challenge 01

--- ms_challenge_01.py
+++ ms_challenge_01.py
@@ -13,5 +13,18 @@
 # Calorie content for Thursday: 1000
 #Start
 
-# this will ask the user for the date
-print("") 
+# this will ask the user what is today's date
+print("What is today's date? ")
+date_today = input()
+# this will ask the user for the number of calories consumed for breakfast
+print("Number of calories for breakfast? ")
+breakfast_cal = int(input())
+# this will ask the user for the number of calories consumed for breakfast
+print("Number of calories for lunch? ")
+lunch_cal = int(input())
+# this will ask the user for the number of calories consumed for breakfast
+print("Number of calories for dinner? ")
+dinner_cal = int(input())
+# sum the total calories
+total_day = breakfast_cal + lunch_cal + dinner_cal
+print("Calorie to total for " + date_today + ": " + str(total_day))
