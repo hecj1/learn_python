c4f3d9ea170e9cb9eed46082d753f8fce3f085f5
James Howell <james.howell@slalom.com>
2020-09-02 15:32:01 -0400

    updating for_loops

--- .vscode/atbs/for_loops.py
+++ .vscode/atbs/for_loops.py
@@ -1,5 +1,10 @@
 total = 0
-for num in range(101):
+for num in range(10):
     total = num + (num - 1)
     print(total)
 print(total)
+
+supplies = ['pens', 'staplers', 'papers', 'rocks', 'flamethrowers']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
