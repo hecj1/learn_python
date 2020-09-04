cfbf231767e10b5f670065d4fea677b5aacf38e6
James Howell <james.howell@slalom.com>
2020-09-03 22:03:40 -0400

    updating zeroDivide with another exception block

--- .vscode/atbs/zeroDivide.py
+++ .vscode/atbs/zeroDivide.py
@@ -10,3 +10,13 @@ print(div42by(2))
 print(div42by(12))
 print(div42by(0))
 print(div42by(1))
+
+print('How many cookies do you have?')
+numCookies = input()
+try:
+    if int(numCookies) >= 4:
+        print('You must be a cookie monster')
+    else:
+        print('You\'re not eating enough cookies')
+except ValueError:
+    print('You did\'t enter a number for your cookies')
